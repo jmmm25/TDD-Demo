5a20c43dde327a07730688f908df90694acfb2b6
김정민 <jmkim9302@gmail.com>
2021-08-05 08:45:27 +0900

    약한 암호면 가입 실패 테스트 코드 Draft 작성

---
+++ src/test/java/chap07/StubWeakPasswordChecker.java
@@ -0,0 +1,9 @@
+package chap07;
+
+public class StubWeakPasswordChecker implements WeakPasswordChecker {
+    private boolean weak;
+
+    public void setWeak(boolean weak) {
+        this.weak = weak;
+    }
+}
